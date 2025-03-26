0f93e568f7f21e532ec5b077b0398043c664003e
Matvey Fedorov <mfgolden@yandex.ru>
2025-03-27 00:53:24 +0300

    adding ability to start the server

--- main.go
+++ main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"log"
+
+	"github.com/MatTwix/RE-minder/config"
+	"github.com/MatTwix/RE-minder/database"
+	"github.com/gofiber/fiber/v3"
+)
 
 func main() {
-	fmt.Println("hello world!")
+	app := fiber.New()
+	cfg := config.LoadConfig()
+
+	database.ConnectDB()
+	defer database.DB.Close()
+
+	if cfg.ENV != "production" {
+		//TODO: CORS allows for dev mode
+	}
+
+	// TODO: make routes setup
+
+	if cfg.ENV == "production" {
+		// TODO: react app static views from dist/ dir
+	}
+
+	if err := app.Listen(":" + cfg.Port); err != nil {
+		log.Fatal("Error starting server: ", err)
+	}
 }
