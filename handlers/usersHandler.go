caf7e327460afbdfe24de37dcd6727a110f49960
Matvey Fedorov <mfgolden@yandex.ru>
2025-03-28 11:16:33 +0300

    adding test handler

---
+++ handlers/usersHandler.go
@@ -0,0 +1,30 @@
+package handlers
+
+import (
+	"context"
+
+	"github.com/MatTwix/RE-minder/database"
+	"github.com/MatTwix/RE-minder/models"
+	"github.com/gofiber/fiber/v3"
+)
+
+func GetUsers(c fiber.Ctx) error {
+	rows, err := database.DB.Query(context.Background(), "SELECT id, name FROM users")
+	if err != nil {
+		return c.Status(500).JSON(fiber.Map{"error": "Error while getting users: " + err.Error()})
+	}
+
+	defer rows.Close()
+
+	var users []models.User
+
+	for rows.Next() {
+		var user models.User
+		if err := rows.Scan(&user.ID, &user.Name); err != nil {
+			return c.Status(500).JSON(fiber.Map{"error": "Error parsing data: " + err.Error()})
+		}
+		users = append(users, user)
+	}
+
+	return c.JSON(users)
+}
