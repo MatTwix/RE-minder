5b80633b59ac5d4b7dc7a0295ff905988c46ba85
Matvey Fedorov <mfgolden@yandex.ru>
2025-03-30 23:12:36 +0300

    blocking usage of testing fields in usersHandler

--- handlers/usersHandler.go
+++ handlers/usersHandler.go
@@ -20,7 +20,7 @@ func GetUsers(c fiber.Ctx) error {
 
 	for rows.Next() {
 		var user models.User
-		if err := rows.Scan(&user.ID, &user.Name); err != nil {
+		if err := rows.Scan(&user.ID); err != nil {
 			return c.Status(500).JSON(fiber.Map{"error": "Error parsing data: " + err.Error()})
 		}
 		users = append(users, user)
