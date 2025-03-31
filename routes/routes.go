26ede3ee1e2db743436091f8b91ae3400ac2575d
Matvey Fedorov <mfgolden@yandex.ru>
2025-03-31 23:25:13 +0300

    updating routes

--- routes/routes.go
+++ routes/routes.go
@@ -6,6 +6,19 @@ import (
 )
 
 func SetupRoutes(app *fiber.App) {
-	users := app.Group("/users")
+	api := app.Group("/api")
+
+	users := api.Group("/users")
 	users.Get("/", handlers.GetUsers)
+	users.Get("/:id", handlers.GetUser)
+	users.Post("/", handlers.CreateUser)
+	users.Put("/:id", handlers.UpdateUser)
+	users.Delete("/:id", handlers.DeleteUser)
+
+	habits := api.Group("/habits")
+	habits.Get("/", handlers.GetHabits)
+	habits.Get("/:id", handlers.GetHabit)
+	habits.Post("/", handlers.CreateHabit)
+	habits.Put("/:id", handlers.UpdateHabit)
+	habits.Delete("/:id", handlers.DeleteHabit)
 }
