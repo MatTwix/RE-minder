9fd1385ff6f0979dac51b68936aa10143bef013c
Matvey Fedorov <mfgolden@yandex.ru>
2025-07-07 13:15:45 +0300

    adding some fields

---
+++ migrations/addIsAdminField.go
@@ -0,0 +1,48 @@
+package migrations
+
+import (
+	"context"
+	"log"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+func AddIsAdminField(DB *pgxpool.Pool) {
+	ctx := context.Background()
+	tx, err := DB.Begin(ctx)
+	if err != nil {
+		log.Fatal("Error starting transaction to add is_admin field: ", err)
+	}
+	defer tx.Rollback(ctx)
+
+	var columnExists bool
+	err = tx.QueryRow(ctx, `
+		SELECT EXISTS (
+			SELECT 1 
+			FROM information_schema.columns 
+			WHERE table_name = 'users' AND column_name = 'is_admin'
+		);
+	`).Scan(&columnExists)
+	if err != nil {
+		log.Fatal("Error checking if is_admin column exists: ", err)
+	}
+
+	if !columnExists {
+		_, err = tx.Exec(ctx, `
+			ALTER TABLE users ADD COLUMN is_admin BOOLEAN DEFAULT FALSE;
+		`)
+		if err != nil {
+			log.Fatal("Error adding is_admin column: ", err)
+		}
+
+		err = tx.Commit(ctx)
+		if err != nil {
+			log.Fatal("Error committing transaction: ", err)
+		}
+
+		log.Println("is_admin field successfully added to users table!")
+	} else {
+		tx.Rollback(ctx)
+		log.Println("is_admin field already exists in users table.")
+	}
+}
