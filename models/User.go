e63b970ecdd0b90ef76323fc866a90e4cf9e9f7e
Matvey Fedorov <mfgolden@yandex.ru>
2025-07-07 13:18:08 +0300

    refactor: add StartDate field to Habit model and IsAdmin field to User model

--- models/User.go
+++ models/User.go
@@ -7,6 +7,7 @@ type User struct {
 	Username   string    `json:"username"`
 	TelegramId *int      `json:"telegram_id,omitempty"`
 	GithubId   *int      `json:"github_id"`
+	IsAdmin    bool      `json:"is_admin"`
 	CreatedAt  time.Time `json:"created_at"`
 	UpdatedAt  time.Time `json:"updated_at"`
 }
